58d518948ca3eb2acbc60b5c71e02000f7e6fb2a
CatalinaP84 <prv_catalina@yahoo.com>
2023-04-01 16:01:29 +0300

    exercise 24 refactoring

--- ex24.py
+++ ex24.py
@@ -15,9 +15,9 @@ print("--------------")
 print(poem)
 print("--------------")
 
-
 five = 10 - 2 + 3 - 6
 print(f"This should be five: {five}")
+
 
 def secret_formula(started):
     jelly_beans = started * 500
@@ -26,7 +26,7 @@ def secret_formula(started):
     return jelly_beans, jars, crates
 
 
-start_point= 10000
+start_point = 10000
 beans, jars, crates = secret_formula(start_point)
 
 # remember that this is another way to format a string
@@ -39,4 +39,4 @@ start_point = start_point / 10
 print("We cand also do that this way:")
 formula = secret_formula(start_point)
 # this is an easy way to apply a list to a format string
-print("We'd have {} beans, {} jars, and {} crates.".format(* formula))
+print("We'd have {} beans, {} jars, and {} crates.".format(*formula))
