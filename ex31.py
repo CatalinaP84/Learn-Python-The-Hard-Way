e5b09da7e743a926470feb9ce3787eb17815af2d
CatalinaP84 <prv_catalina@yahoo.com>
2023-07-30 14:40:40 +0300

    exercise 31 small changes

--- ex31.py
+++ ex31.py
@@ -11,16 +11,16 @@ if door == "1":
 
     bear = input("> ")
 
-if bear == "1":
-    print("The bear eats your face off. Good job!")
-elif bear == "2":
-    print("The bear eats your legs off. Good job!")
-else:
-    print(f"Well, doing {bear} is probably better.")
-    print("Bear runs away.")
+    if bear == "1":
+        print("The bear eats your face off. Good job!")
+    elif bear == "2":
+        print("The bear eats your legs off. Good job!")
+    else:
+        print(f"Well, doing {bear} is probably better.")
+        print("Bear runs away.")
 
 elif door == "2":
-    print("You stare into the endless abyss at Cthulhu's ret.")
+    print("You stare into the endless abyss at Cthulhu's retina.")
     print("1. Blueberries.")
     print("2. Yellow jacket clothespins.")
     print("3. Understanding revolvers yelling melodies.")
@@ -28,11 +28,11 @@ elif door == "2":
     insanity = input("> ")
 
     if insanity == "1" or insanity == "2":
-    print("Your body survives powered by a mind of jello")
-    print("Good job!")
+        print("Your body survives powered by a mind of jello.")
+        print("Good job!")
     else:
-    print("The insanity rots your eyes into a pool of mud")
-    print("Good job!")
+        print("The insanity rots your eyes into a pool of muck.")
+        print("Good job!")
 
 else:
     print("You stumble around and fall on a knife and die. Good job!")
