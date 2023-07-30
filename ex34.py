bd98136cc80e2f810d36dd7fdeb4b8f7163d518d
CatalinaP84 <prv_catalina@yahoo.com>
2023-07-30 19:21:11 +0300

    exercise 34

---
+++ ex34.py
@@ -0,0 +1,11 @@
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+bear = animals[0]
+
+print("The animal at 1 is a", animals[1])
+print("The third animal is a", animals[2])
+print("The first animal is a", animals[0])
+print("The animal at 3 is a", animals[3])
+print("The fifth animal is a", animals[4])
+print("The animal at 2 is a", animals[2])
+print("The sixth animal is a", animals[5])
+print("The animal at 4 is a", animals[4])
